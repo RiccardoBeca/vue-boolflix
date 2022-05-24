e64506064746d8d9b9b603a69e8fe1e55ebb809c
RiccardoBeca <riccardobeca@gmail.com>
2022-05-24 10:09:23 +0100

    milestone 2 complete

--- src/components/FilmCardComp.vue
+++ src/components/FilmCardComp.vue
@@ -2,7 +2,13 @@
   <div class="my-5">
     <h1>{{card.title}}</h1>
     <h2>{{card.original_title}}</h2>
-    <h3>{{card.original_language}}</h3>
+    <h3 v-if="card.original_language == 'en'">
+      <img src="../assets/img/Flag_of_the_United_Kingdom.svg.png" alt="">
+    </h3>
+     <h3 v-else-if="card.original_language == 'it'">
+      <img src="../assets/img/index.png" alt="">
+    </h3>
+    <h3 v-else>Language:{{card.original_language}}</h3>
     <h4>{{card.vote_average}}</h4>
     <hr>
   </div>
